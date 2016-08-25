350f5d58b0bf6c28875b13e814f02ada7a752ee5
David1367 <pedro.571.david@gmail.com>
2016-08-25 15:52:48 -0400

    Initial Set up

---
+++ DBSP.RememberMe.Identity.Server/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DBSP.RememberMe.Identity.Server
+{
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext()
+        : base("DefaultConnection", throwIfV1Schema: false)
+    {
+    }
+
+    public static ApplicationDbContext Create()
+    {
+      return new ApplicationDbContext();
+    }
+  }
+}
+
